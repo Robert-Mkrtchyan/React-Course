cd828c839d7d2c7c279f9b8e62cec8190a4b1a6e
Robert-Mkrtchyan <rob.0201@mail.ru>
2024-06-20 22:08:03 +0400

    implement card and button component

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import Card  from './components/card';
+import Button from './components/button';
+
+function App () {
+    return (
+        <div>
+            <h2>App</h2>
+            <Card />
+
+            <hr />
+
+            <Button />
+        </div>
+    )
+}
+
+export default App;
---
+++ my-app/src/components/button/index.js
@@ -0,0 +1,10 @@
+
+const button = () => {
+    return (
+        <div>
+            <h2>Button</h2>
+        </div>
+    )
+}
+
+export default button;
